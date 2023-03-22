1a569d9c58f5355af77be5f6ca83c03fffc0ac6b
svetlana <milkshake9273@gmai.com>
2023-03-22 21:38:27 +1000

    method for input array

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//1 ввод массива с клавиатуры
+static string[] InputStringArray()
+{
+  Console.WriteLine("Введите размер массива");
+  int size = Convert.ToInt32(Console.ReadLine());
+  string[] array = new string[size];
+  for (int i = 0; i < size; i++)
+  {
+    Console.WriteLine("Введите {0}-й элемент", i + 1);
+    array[i] = Console.ReadLine();
+  }
+  return array;
+}
+
+// печать массива
+void PrintArray (string [] array)
+{
+  System.Console.WriteLine(String.Join(", ", array));
+}
+
+PrintArray(InputStringArray());
+ 
+
